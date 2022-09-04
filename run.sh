4363b6141efdff9619fd29536aedb516552d241e
EC2 Default User <ec2-user@ip-172-31-87-207.ec2.internal>
2022-09-04 14:11:06 +0000

    updated for hook

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 !#/bin/bash
 # You can add these commands to simple shell script so that you dont need to run them each time
-git add .
-git commit -m "updated"
-git push origin master
+echo "Print this"
